c6781ab774dd479eab14a5ac09be2ff88e6c1191
Rudy Ravelin <rudy@noukod.com>
2018-10-15 18:57:17 +0000

    automatically backed up by learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,4 +1,5 @@
 class Vehicle
+  
   attr_accessor :wheel_size, :wheel_number
 
   def initialize(wheel_size, wheel_number)
@@ -11,7 +12,7 @@ def go
 end
 
 def fill_up_tank
-  "filling up"
+  "filling up!"
 end
 
 end
